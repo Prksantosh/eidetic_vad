08fc8d9007352d941720cc6437fb896beed6bb34
Prksantosh <santosh.prk22@gmail.com>
2026-03-12 15:46:29 +0530

    Update decoder.py

--- models/decoder.py
+++ models/decoder.py
@@ -2,24 +2,32 @@
 import torch.nn as nn
 from models.ae_rhc_layer import AERHCLayer
 
-class Decoder(nn.Module):
-    def __init__(self, base_channels, out_channels):
+###############################################
+# Decoder Stage
+###############################################
+class DecoderStage(nn.Module):
+
+    def __init__(self, in_ch, out_ch):
+
         super().__init__()
 
-        self.up1 = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.layer1 = AERHCLayer(base_channels*4, base_channels*2)
+        self.aerhc = AE_RHCNet(in_ch)
 
-        self.up2 = nn.Upsample(scale_factor=2, mode='bilinear')
-        self.layer2 = AERHCLayer(base_channels*2, base_channels)
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False),
 
-        self.final = nn.Conv2d(base_channels, out_channels, 3, padding=1)
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+
+            nn.BatchNorm2d(out_ch),
+
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        x = self.up1(x)
-        x = self.layer1(x)
 
-        x = self.up2(x)
-        x = self.layer2(x)
+        x = self.aerhc(x)
 
+        x = self.up(x)
 
-        return self.final(x)
+        return x
+
