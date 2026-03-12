3cfe85c0abe0c4b67726e5adb81f49fec40dd872
Prksantosh <santosh.prk22@gmail.com>
2026-03-12 15:45:52 +0530

    Update encoder.py

--- models/encoder.py
+++ models/encoder.py
@@ -1,25 +1,28 @@
 import torch.nn as nn
 from models.rhc_layer import RHCLayer
 
-class Encoder(nn.Module):
-    def __init__(self, in_channels, base_channels):
+###############################################
+# Encoder Stage
+###############################################
+class EncoderStage(nn.Module):
+
+    def __init__(self, in_ch, out_ch):
+
         super().__init__()
 
-        self.layer1 = RHCLayer(in_channels, base_channels)
-        self.pool1 = nn.MaxPool2d(2)
+        self.rhc = RHCBlock(in_ch)
 
-        self.layer2 = RHCLayer(base_channels, base_channels*2)
-        self.pool2 = nn.MaxPool2d(2)
-
-        self.layer3 = RHCLayer(base_channels*2, base_channels*4)
+        self.down = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, stride=2, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.pool1(x)
 
-        x = self.layer2(x)
-        x = self.pool2(x)
-
-        x = self.layer3(x)
+        x = self.rhc(x)
+        x = self.down(x)
 
         return x
+
+
